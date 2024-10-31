e1e7f7f1dff398b927c4ef362ae7c20b90425326
domenatthawat <144078142+domenatthawat@users.noreply.github.com>
2024-10-31 16:15:28 -0600

    Lab 3: Props and State Management

---
+++ Lab3/Props-and-StateManagement/app/ToDoList.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Pressable, View, Text, ScrollView } from 'react-native';
+
+function ToDoList({ tasks }) {
+  return (
+    <ul>
+      {tasks.map((task, index) => (
+        <li key={index}>{task}</li>
+      ))}
+    </ul>
+  );
+}
+const styles = StyleSheet.create({
+    task: {
+      padding: 10,
+      borderBottomWidth: 1,
+      borderColor: '#ccc',
+    },
+    completed: {
+      backgroundColor: '#e0e0e0',
+    }
+  });
+
+  export default ToDoList;
---
+++ Lab3/Props-and-StateManagement/app/app.jsx
@@ -0,0 +1,35 @@
+/**
+ * My To Do List App
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  ScrollView,
+  Text,
+  Pressable,
+  TextInput,
+  Button
+} from 'react-native';
+
+import ToDoList from './ToDoList';
+import ToDoForm from './ToDoForm';
+import { useState } from 'react';
+
+function App() {
+
+  const [tasks, setTasks] = useState(["Do laundry", "Go to gym", "Walk dog"])
+
+  return (
+    <SafeAreaView>
+      <ToDoList tasks={tasks} />
+      <ToDoForm />
+    </SafeAreaView>
+  );
+}
+
+export default App;
